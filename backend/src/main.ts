e84a027439a4e3b73a1f3406a3054556de540810
Franscico Ruiz <franciscoruiznietoo@gmail.com>
2025-02-09 23:04:52 +0100

    Enable CORS for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://humble-superhero-api-q0qc.onrender.com/',
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type',
+  });
   await app.listen(process.env.PORT ?? 3000);
   console.log('Access Point running on http://localhost:3000');
 }
